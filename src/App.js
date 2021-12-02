385234ff5decfaef14f125739cd80183c99c48f2
Aswebilius <81036333+Aswebilius@users.noreply.github.com>
2021-12-02 00:27:10 -0500

    VideoDetail component complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { SearchBar, VideoList, VideoDetail } from './components';
 import youtube from './api/youtube';
 
 class App extends React.Component {
+    state = {
+        video: [ ],
+        selectedVideo: null,
+    }
+
     handleSubmit = async (searchTerm) => {
         const response = await youtube.get('search', {
             params: {
@@ -15,22 +20,22 @@ class App extends React.Component {
                 q: searchTerm,
             }
         });
-        console.log(response.data.items);
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
     }
     render() {
+        const { selectedVideo } = this.state;
         return (
             <Grid justifyContent="center" container spacing={10}>
                 <Grid item xs={12}>
                     <Grid container spacing={10}>
+                        <Grid item xs={12}>
 
                         <SearchBar onFormSubmit={this.handleSubmit} />
 
-                        <Grid item xs={12}>
-
-                            <VideoDetail />
-
                         </Grid>
                         <Grid item xs={8}>
+
+                            <VideoDetail video={selectedVideo}/>
 
                         </Grid>
                         <Grid item xs={4}>
